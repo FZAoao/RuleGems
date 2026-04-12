07b5a9ba4c704f1411ba5ac6e7a097b50f8d8675
angushushu <angushushu@outlook.com>
2026-04-12 14:44:06 -0400

    chore: remove accidentally added python scripts, test code and config folders

--- test.java
+++
@@ -1,6 +0,0 @@
-import org.incendo.cloud.parser.standard.StringParser;
-public class test {
-    public static void main(String[] args) {
-        System.out.println(java.util.Arrays.toString(StringParser.StringMode.values()));
-    }
-}
